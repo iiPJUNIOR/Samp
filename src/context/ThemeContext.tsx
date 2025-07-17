ee6ea84cd1d378217fc3d1f015d370236436df48
iiPJUNIOR <junior.8350i@gmail.com>
2025-07-17 12:02:12 -0300

    versão inicial

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -10,7 +10,7 @@ type ThemeContextType = {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const [theme, setTheme] = useState<Theme>("light");
 
   useEffect(() => {
     // Aplica o tema ao elemento HTML
