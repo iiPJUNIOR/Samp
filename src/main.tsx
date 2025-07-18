40e3d5c52149b038ad79ddc050ccc57e3965ab8a
iiPJUNIOR <junior.8350i@gmail.com>
2025-07-18 14:08:33 -0300

    Atualização do repositório com build final

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-// Aplicar tema escuro como padrão
-document.documentElement.classList.add('dark');
-
-createRoot(document.getElementById("root")!).render(<App />);
--- src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-import ProcessFlowApp from '@/components/ProcessFlowApp';
-
-const Index = () => {
-  return <ProcessFlowApp />;
-};
-
-export default Index;
